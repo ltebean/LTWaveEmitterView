9b387bfc32d1285b2c020f8ec5b90c13f13238d1
ltebean <yucong1118@gmail.com>
2016-05-09 14:26:06 +0800

    Initial Commit

---
+++ LTWaveEmitterView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LTWaveEmitterView
+//
+//  Created by ltebean on 16/5/9.
+//  Copyright © 2016年 io.ltebean. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
